5e24ff15c25e15e85a2b62b39a32123897ca2e1b
Alexander Belikov <belikov.it@gmail.com>
2021-11-03 09:58:56 +0500

    HexToRGB

--- src/info/belikov/codewars/k4/HexToRGB.java
+++ src/info/belikov/codewars/k4/HexToRGB.java
@@ -8,8 +8,4 @@ public class HexToRGB {
     public static int[] hexStringToRGB(String hex) {
         return new int[] {Integer.parseInt(hex.substring(1,3), 16), Integer.parseInt(hex.substring(3,5), 16), Integer.parseInt(hex.substring(5,7), 16)};
     }
-
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(hexStringToRGB("#FF9933")));
-    }
 }
